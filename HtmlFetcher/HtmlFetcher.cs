fd1d9a697487f18ebd77f21c2c55a418924ceee3
Mert Can Erdem <mrtcnrdmii@gmail.com>
2024-04-25 14:22:58 +0300

    Initial commit

---
+++ HtmlFetcher/HtmlFetcher.cs
@@ -0,0 +1,33 @@
+﻿namespace HtmlFetcher
+{
+    public class HtmlFetcher : IDisposable
+    {
+        private readonly HttpClient client;
+
+        public HtmlFetcher()
+        {
+            client = new HttpClient();
+        }
+
+        public async Task<string> FetchHtml(string url)
+        {
+            HttpResponseMessage response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode(); // Hata durumunda istisna fırlatır
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                client.Dispose(); // HttpClient nesnesini temizle
+            }
+        }
+    }
+}
---
+++ HtmlFetcher/Program.cs
@@ -0,0 +1,13 @@
+﻿using (var htmlFetcher = new HtmlFetcher.HtmlFetcher())
+{
+    await Console.Out.WriteLineAsync("Url giriniz: ");
+    string? url = Console.ReadLine();
+
+    if (string.IsNullOrEmpty(url))
+    {
+        url = "https://www.google.com";
+    }
+
+    string html = await htmlFetcher.FetchHtml(url);
+    Console.WriteLine(html);
+}
